3e412590e8887a66406170bcad28a1d0a50a0a05
adiezsanchez <adiez.biotech@gmail.com>
2025-06-16 15:57:12 +0200

    Quick spot detection Spotiflow trial

---
+++ utils.py
@@ -0,0 +1,74 @@
+from pathlib import Path
+import czifile
+import nd2
+import tifffile
+import napari
+import numpy as np
+import os
+import pandas as pd
+from skimage import measure
+from scipy.ndimage import binary_erosion
+import pyclesperanto_prototype as cle
+import warnings
+
+cle.select_device("RTX")
+
+def get_gpu_details():
+    devices = device_lib.list_local_devices()
+    for device in devices:
+        if device.device_type == 'GPU':
+            print(f"Device name: {device.name}")
+            print(f"Device type: {device.device_type}")
+            print(f"GPU model: {device.physical_device_desc}")
+
+def list_images (directory_path, format=None):
+
+    # Create an empty list to store all image filepaths within the dataset directory
+    images = []
+
+    # If manually defined format
+    if format:
+        for file_path in directory_path.glob(f"*.{format}"):
+            images.append(str(file_path))
+
+    else:
+        # Iterate through the .czi and .nd2 files in the directory
+        for file_path in directory_path.glob("*.czi"):
+            images.append(str(file_path))
+            
+        for file_path in directory_path.glob("*.nd2"):
+            images.append(str(file_path))
+
+    return images
+
+def read_image (image, slicing_factor_xy):
+    """Read raw image microscope files (.nd2), apply downsampling if needed and return filename and a numpy array"""
+
+    # Read path storing raw image and extract filename
+    file_path = Path(image)
+    filename = file_path.stem
+
+    # Extract file extension
+    extension = file_path.suffix
+
+    if extension == ".nd2":
+        # Read stack from .nd2 (z, ch, x, y) or (ch, x, y)
+        img = nd2.imread(image)
+        
+    else:
+        print ("Implement new file reader")
+
+    print(f"\n\nImage analyzed: {filename}")
+    print(f"Original Array shape: {img.shape}")
+
+    # Apply slicing trick to reduce image size (xy resolution)
+    try:
+        img = img[:, ::slicing_factor_xy, ::slicing_factor_xy]
+    except IndexError as e:
+        print(f"Slicing Error: {e}")
+        print(f"Slicing parameters: Slicing_XY:{slicing_factor_xy}")
+
+    # Feedback for researcher
+    print(f"Compressed Array shape: {img.shape}")
+
+    return img, filename
